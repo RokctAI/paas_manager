13d27fbb51a064e8feae695a922667ae14ad8cbb
Rendani Sinyage <sinyage@gmail.com>
2026-02-13 16:05:36 +0200

    Update app_constants.dart

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -3,9 +3,9 @@ import 'infrastructure/services/enums.dart';
 class AppConstants {
   AppConstants._();
 
-  static const bool isDemo = false;
-  static const bool autoTrn = true;
-  static bool appStoreMode = false;
+  static const bool isDemo = bool.fromEnvironment('IS_DEMO');
+  static const bool autoTrn = bool.fromEnvironment('AUTO_TRN');
+  static const bool appStoreMode = bool.fromEnvironment('APP_STORE_MODE');
 
   ////Water
   static const bool enableWaterMeterGallerySelection = true;
@@ -16,32 +16,32 @@ class AppConstants {
   static const String heroTagIncomePage = 'heroTagIncomePage';
   static const String heroTagListNotification = 'heroTagListNotification';
   /// api urls
-  /// api urls
-  static const String adminPageUrl = baseUrl;
-  static String imageBaseUrl = '$baseUrl/storage/images';
-  static String chatGpt = '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';
-  static String webUrl = 'https://web.juvo.app';
+  static const String baseUrl = String.fromEnvironment('BASE_URL');
+  static const String adminPageUrl = String.fromEnvironment('ADMIN_URL');
+  static String imageBaseUrl = const String.fromEnvironment('IMAGE_BASE_URL');
+  static String chatGpt = const String.fromEnvironment('CHAT_GPT_KEY');
+  static String webUrl = const String.fromEnvironment('WEB_URL');
 
 
   /// auth phone fields
-  static bool isSpecificNumberEnabled = false;
-  static bool isNumberLengthAlwaysSame = true;
-  static String countryCodeISO = 'ZA';
-  static bool showFlag = true;
-  static bool showArrowIcon = true;
-  static SignUpType signUpType = SignUpType.phone;
+  static bool isSpecificNumberEnabled = const bool.fromEnvironment('IS_SPECIFIC_NUMBER_ENABLED');
+  static bool isNumberLengthAlwaysSame = const bool.fromEnvironment('IS_NUMBER_LENGTH_ALWAYS_SAME');
+  static String countryCodeISO = const String.fromEnvironment('COUNTRY_ISO');
+  static bool showFlag = const bool.fromEnvironment('SHOW_FLAG');
+  static bool showArrowIcon = const bool.fromEnvironment('SHOW_ARROW_ICON');
+  static SignUpType get signUpType => SignUpType.values.byName(const String.fromEnvironment('SIGN_UP_TYPE'));
 
 
 
   /// location
-  static double demoLatitude = -22.34058;
-  static double demoLongitude = 30.01341;
+  static double demoLatitude = double.parse(const String.fromEnvironment('DEMO_LATITUDE'));
+  static double demoLongitude = double.parse(const String.fromEnvironment('DEMO_LONGITUDE'));
   static const double pinLoadingMin = 0.116666667;
   static const double pinLoadingMax = 0.611111111;
 
   /// demo app info
-  static const String demoSellerLogin = '';
-  static const String demoSellerPassword = '';
+  static const String demoSellerLogin = String.fromEnvironment('DEMO_SELLER_LOGIN');
+  static const String demoSellerPassword = String.fromEnvironment('DEMO_SELLER_PASSWORD');
 
   ////QRCodes for Water Service
   static const Map<String, String> STEP_CODES = {
